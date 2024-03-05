a45fa37f7d87094c0c57eeebd523e281413929ec
harry <harry@onlinebizsoft.com>
2024-03-05 15:10:15 +0700

    test  change name

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -20,7 +20,7 @@ import {
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
+  name: "Thanh Nguyen Trung",
   initials: "BJ",
   location: "Wrocław, Poland, CET",
   locationLink: "https://www.google.com/maps/place/Wrocław",
